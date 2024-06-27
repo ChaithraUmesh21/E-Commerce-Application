30099de9b2457488f905b367c97accdb059e8075
ChaithraUmesh21 <138147671+ChaithraUmesh21@users.noreply.github.com>
2024-06-27 16:27:58 +0100

    code added

---
+++ Web/server/routes/products.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+const Product = require('../models/Product');
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage });
