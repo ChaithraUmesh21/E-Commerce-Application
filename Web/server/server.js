c5ccf7f4fdac4628cbbc9fdb48c4aeecb60ab073
ChaithraUmesh21 <138147671+ChaithraUmesh21@users.noreply.github.com>
2024-07-06 19:16:34 +0100

    bugfix

--- Web/server/server.js
+++ Web/server/server.js
@@ -28,9 +28,12 @@ app.use('/api/categories', require('./routes/categories'));
 const PORT = process.env.PORT || 5000;
 
 
-app.use("/api", require(",/frontend.build"));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
