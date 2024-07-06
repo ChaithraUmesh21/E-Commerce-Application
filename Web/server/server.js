b3a33df58484792ad868b294f20ea24c416d2a12
ChaithraUmesh21 <138147671+ChaithraUmesh21@users.noreply.github.com>
2024-07-06 17:26:15 +0100

    hosting web page

--- Web/server/server.js
+++ Web/server/server.js
@@ -27,4 +27,10 @@ app.use('/api/categories', require('./routes/categories'));
 
 const PORT = process.env.PORT || 5000;
 
+
+app.use("/api", require(",/frontend.build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "src", "index.js"))
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
